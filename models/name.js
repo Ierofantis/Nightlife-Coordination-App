4cc34675277b5f253ca1dd59ef997209c43a298f
Theodore P <evil69669@gmail.com>
2016-08-02 20:23:36 +0300

    models

---
+++ models/name.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+var nameSchema = mongoose.Schema({
+  name: { type: String }  
+});
+
+
+var Name = mongoose.model("Name", nameSchema);
+
+module.exports = Name;
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var express = require('express');
 //var app = express.app();
 //var auth = jwt({secret: 'SECRET', userProperty: 'payload'})
 var mongoose = require('mongoose');
+var Name = require("./models/name");
 var yelp = require("node-yelp");
 //var passport = require('passport');
 var Reg = require("./models/register");
@@ -12,13 +13,11 @@ var Reg = require("./models/register");
 //var Location = require("./models/Location");
 module.exports = function(app, passport) {
     var express = require('express');
-//var jwt = require('express-jwt');
-//var app = express.app();
-//var auth = jwt({secret: 'SECRET', userProperty: 'payload'})
-var mongoose = require('mongoose');
-var yelp = require("node-yelp");
-//var passport = require('passport');
-var Reg = require("./models/register");
+
+    var mongoose = require('mongoose');
+    var yelp = require("node-yelp");
+    //var passport = require('passport');
+    var Reg = require("./models/register");
    //  app.get('/', function(req, res, next) {
    //  res.render('index3', { title: 'Express' });
    // });
@@ -31,13 +30,13 @@ var Reg = require("./models/register");
         res.render('fail.ejs'); // load the index.ejs file
      });
 
-    app.post('/', isLoggedIn, function(req, res, next) { 
-  var r = {re:req.body.location};
- var client = yelp.createClient({
-   oauth: {
+    app.post('/', function(req, res, next) { 
+    var r = {re:req.body.location};
+    var client = yelp.createClient({
+    oauth: {
      "consumer_key": "07BSeMz5vajMDoPc1i02ng",
-    "consumer_secret": "4rBHePhlcRH-SEkbmHRKLlLLbg0",
-    "token": "8i9BMfEh8XttSzoUw-fb8T4C_tk6BVWH",
+     "consumer_secret": "4rBHePhlcRH-SEkbmHRKLlLLbg0",
+     "token": "8i9BMfEh8XttSzoUw-fb8T4C_tk6BVWH",
      "token_secret": "DYEUv11n8qjEVy0hysCeyUCjq58"
    },
   
@@ -55,9 +54,6 @@ client.search({
    var location = data.region;
 
 
- 
-  // for (var i=0;i<businesses.length;i++){
-  // console.log(businesses[i].name);
  res.render("index2", { data:data});
      });
   }); 
@@ -65,18 +61,37 @@ client.search({
    app.get('/signup', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
-
-    // route for login form
-    // route for processing the login form
-    // route for signup form
-    // route for processing the signup form
-
-    // route for showing the profile page
+    
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
+
+    app.post("/submit",isLoggedIn, function(req, res, next) {
+        
+        var name = req.body.name;
+           
+        Name.create(name,function(err,names) {
+         
+        res.redirect('/profile/${name}');
+
+      });
+    });
+
+    app.get("/profile/:name", function(req, res) {
+         var name = req.params.name;
+                
+       
+
+     Name.find({ name: req.params.name
+    }, function(err, data) {
+       if (err) { return next(err); }
+      
+       res.render("titles", { data: data});
+     });
+    });
+
 
     // route for logging out
     app.get('/logout', function(req, res) {
@@ -84,28 +99,16 @@ client.search({
         res.redirect('/');
     });
 
-    // facebook routes
-    // twitter routes
-
-    // =====================================
-    // GOOGLE ROUTES =======================
-    // =====================================
-    // send to google to do the authentication
-    // profile gets us their basic information including their name
-    // email gets their emails
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
     // the callback after google has authenticated the user
     app.get('/auth/google/callback',
             passport.authenticate('google', {
-                    successRedirect : '/',
+                    successRedirect : '/profile',
                     failureRedirect : '/fail'
             }));
 
-};
-
-
-   
+};   
   
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
