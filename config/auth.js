6fdfc035af819931edd353273d6b4dccda31a874
Theodore P <evil69669@gmail.com>
2016-08-04 15:30:50 +0300

    heroku

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
         'googleAuth' : {
-        'clientID'      : '603682516969-nvgvtk0g6uau6nhofmg5fbc9b5ochhl2.apps.googleusercontent.com',
-        'clientSecret'  : 'KYj9cxz2UPCgOuy7Za0wiLBK',
+        'clientID'      : '603682516969-6gp8upn8lk2ft7lcqofh3nmi0qfubso1.apps.googleusercontent.com',
+        'clientSecret'  : '0HZnVT-MknVQ57EtrDOjOhfJ',
         'callbackURL'   : 'https://nightcord.herokuapp.com/auth/google/callback'
     }
 
