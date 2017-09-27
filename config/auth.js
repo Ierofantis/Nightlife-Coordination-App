cfa947ea7f355f44b9478040ff18ef4334cfbdbc
Ierofantis <evil69669@gmail.com>
2017-09-27 16:30:53 +0300

    heroku

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
         'googleAuth' : {
         'clientID'      : '70609776144-o0irhapi4q6j15blqgkahutlkmfc7ntp.apps.googleusercontent.com',
         'clientSecret'  : 'Ic7zj5jj2S1wTqGz_98vjYso',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://nightlifeamerica.herokuapp.com/auth/google/callback'
     }
 };
